59e5704ec54181fb944c9ea650a7013137caa4d4
GuiMonteir0 <guilherme.a.monteiro@tecnico.ulisboa.pt>
2020-11-24 17:09:26 +0000

    signup, login & logout done (needs testing)

--- src/app/_services/authentication/firebase-auth.service.ts
+++ src/app/_services/authentication/firebase-auth.service.ts
@@ -1,10 +1,47 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {Observable} from 'rxjs';
+import firebase from 'firebase';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseAuthService {
+  user: Observable<firebase.User>;
+  constructor(private firebaseAuth: AngularFireAuth) {
+    this.user = firebaseAuth.authState;
+  }
+  signup(email: string, password: string): void {
+    this.firebaseAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then(value => {
+        console.log('Registado com sucesso!', value);
+      })
+      .catch(err => {
+        console.log('Erro:', err.message);
+      });
+  }
 
-  constructor(private angularFirestore: AngularFirestore) { }
+
+  login(email: string, password: string): void {
+    this.firebaseAuth
+      .signInWithEmailAndPassword(email, password)
+      .then(value => {
+        console.log('Login efetuado com sucesso');
+      })
+      .catch(err => {
+        console.log('Erro:', err.message);
+      });
+  }
+
+  logout(): void {
+    this.firebaseAuth.signOut();
+  }
+
 }
+
+
+
+
