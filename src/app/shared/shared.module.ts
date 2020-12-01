fbaed7749b61e36c2abd7ce3298cce86c3b91853
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2020-12-01 21:26:43 +0000

    Started join modal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { GoTopComponent } from './go-top/go-top.component';
+import { JoinModalComponent } from './join-modal/join-modal.component';
 
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent, GoTopComponent],
+  declarations: [NavbarComponent, FooterComponent, GoTopComponent, JoinModalComponent],
     exports: [
         FooterComponent,
         NavbarComponent,
