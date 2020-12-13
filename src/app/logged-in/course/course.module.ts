95a1dda62cb161bd64ff987b31d811ce8076b699
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2020-12-13 18:00:00 +0000

    Enrolling course

--- src/app/logged-in/course/course.module.ts
+++ src/app/logged-in/course/course.module.ts
@@ -5,15 +5,20 @@ import { CourseRoutingModule } from './course-routing.module';
 import { CourseComponent } from './course/course.component';
 import {ProfileModule} from '../profile/profile.module';
 import {GigModule} from '../gig/gig.module';
+import {EnrolComponent} from './enrol/enrol.component';
+import {FormsModule} from '@angular/forms';
+import {SharedModule} from '../../shared/shared.module';
 
 
 @NgModule({
-  declarations: [CourseComponent],
+  declarations: [CourseComponent, EnrolComponent],
   imports: [
     CommonModule,
     CourseRoutingModule,
     ProfileModule,
-    GigModule
+    GigModule,
+    FormsModule,
+    SharedModule
   ]
 })
 export class CourseModule { }
--- src/app/logged-in/course/enrol/enrol.component.ts
+++ src/app/logged-in/course/enrol/enrol.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../../_domain/User';
+import {Course} from '../../../_domain/Course';
+import {ActivatedRoute} from '@angular/router';
+import {FirebaseService} from '../../../_services/firebase.service';
 
 @Component({
   selector: 'app-enrol',
@@ -7,9 +11,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnrolComponent implements OnInit {
 
-  constructor() { }
+  course: Course = {
+    artistID: '',
+    category: '',
+    description: '',
+    id: '',
+    imagesURL: [],
+    name: '',
+    pitch: '',
+    duration: '',
+    price: 0,
+    timesSold: 0,
+    timestamp: 0,
+    list: null,
+    reviews: null,
+    rate: 0
+  };
+
+  artist: User = {title: '', avatar: '', handler: '', interests: [], joiningTimestamp: 0, name: '', type: '', uid: ''};
+
+  paymentInfo = {
+    cardNumber: null,
+    expirationDate: null,
+    securityCode: null,
+    firstName: null,
+    lastName: null,
+  };
+
+  options = {
+    visa: true,
+    mastercard: false,
+    paypal: false
+  };
+
+  constructor(private router: ActivatedRoute, private firebaseService: FirebaseService) { }
 
   ngOnInit(): void {
+    this.router.parent.params.subscribe(params => {
+      this.course.id = params.cid;
+
+      this.firebaseService.getCourseInfo(this.course.id).then(courseInfo => {
+        this.course.name = courseInfo.name;
+        this.course.category = courseInfo.category;
+        this.course.pitch = courseInfo.pitch;
+        this.course.description = courseInfo.description;
+        this.course.duration = courseInfo.duration;
+        if (courseInfo.list) this.course.list = courseInfo.list;
+        this.course.price = courseInfo.price;
+        this.course.imagesURL = courseInfo.imagesURL;
+        if (courseInfo.rate) this.course.rate = courseInfo.rate;
+        this.course.timesSold = courseInfo.timesSold;
+        this.course.timestamp = courseInfo.timestamp;
+        this.course.artistID = courseInfo.artistID;
+        this.course.reviews = courseInfo.reviews;
+
+        this.firebaseService.getUserInfo(this.course.artistID).then(artistInfo => {
+          this.artist.name = artistInfo.name;
+          this.artist.avatar = artistInfo.avatar;
+          this.artist.title = artistInfo.title;
+        });
+      });
+
+    });
   }
 
 }
